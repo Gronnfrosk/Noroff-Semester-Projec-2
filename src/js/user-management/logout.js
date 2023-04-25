a4b88716dfde897a913d3cf797fab217ac35d489
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2023-04-25 17:58:27 +0200

    if buttons log out when clicked

--- src/js/user-management/logout.js
+++ src/js/user-management/logout.js
@@ -1,24 +1,29 @@
+const logoutButton = document.querySelector("#logout");
+const logoutButtonFooter = document.querySelector("#logoutFooter");
+
 /**
  * This function initiate when logout button is clicked and the user logs out by removing JSON Web Tokens.
  * @param {Element} logoutButton This is a html element where a button is displayed.
  */
+
 export function logout() {
-	const logoutButton = document.querySelector("#logout");
-	const logoutButtonFooter = document.querySelector("#logoutFooter");
+	if (logoutButton) {
+		logoutButton.addEventListener("click", () => {
+			localStorage.removeItem("token");
+			localStorage.removeItem("profile");
+			if (location.pathname !== "/Noroff-Semester-Project-2/index.html") {
+				window.location.href = "/Noroff-Semester-Project-2/index.html";
+			}
+		});
+	}
 
-	logoutButton.addEventListener("click", () => {
-		localStorage.removeItem("token");
-		localStorage.removeItem("profile");
-		if (location.pathname !== "/Noroff-Semester-Project-2/index.html") {
-			window.location.href = "/Noroff-Semester-Project-2/index.html";
-		}
-	});
-
-	logoutButtonFooter.addEventListener("click", () => {
-		localStorage.removeItem("token");
-		localStorage.removeItem("profile");
-		if (location.pathname !== "/Noroff-Semester-Project-2/index.html") {
-			window.location.href = "/Noroff-Semester-Project-2/index.html";
-		}
-	});
+	if (logoutButtonFooter) {
+		logoutButtonFooter.addEventListener("click", () => {
+			localStorage.removeItem("token");
+			localStorage.removeItem("profile");
+			if (location.pathname !== "/Noroff-Semester-Project-2/index.html") {
+				window.location.href = "/Noroff-Semester-Project-2/index.html";
+			}
+		});
+	}
 }
