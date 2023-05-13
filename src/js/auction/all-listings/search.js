c3ce3b07fe463e9a1cb4ff70fa6aeb047b457a66
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2023-05-13 13:50:55 +0200

    Added search button

--- src/js/auction/all-listings/search.js
+++ src/js/auction/all-listings/search.js
@@ -1,11 +1,24 @@
 import { showCards } from "./auction_card_template.js";
 
-const search = document.querySelector("#search_form");
+const search = document.querySelector("#search_form button");
 const input = document.querySelector("#search_input");
 const container = document.querySelector("#card-container");
 const moreLoadBtn = document.querySelector("#moreBtn");
 
 export function searchItems(items) {
+	search.addEventListener("click", function (e) {
+		e.preventDefault();
+		container.innerHTML = "";
+		moreLoadBtn.classList.add("d-none");
+		const searchInput = input.value.trim().toLowerCase();
+		const searchFiltered = items.filter(function (items) {
+			if (items.title.toLowerCase().trim().includes(searchInput)) {
+				return true;
+			}
+		});
+		showCards(searchFiltered);
+	});
+
 	input.addEventListener("keypress", function (e) {
 		if (e.key === "Enter") {
 			e.preventDefault();
