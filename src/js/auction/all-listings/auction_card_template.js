9b98139a0ea4a7a419d5d377c8bbb5387613af56
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2023-05-15 16:08:22 +0200

    links

--- src/js/auction/all-listings/auction_card_template.js
+++ src/js/auction/all-listings/auction_card_template.js
@@ -46,7 +46,7 @@ export async function showCards(items) {
 			}
 
 			//Specific link for home page
-			if (location.pathname !== "/html/profile.html" || location.pathname !== "/Noroff-Semester-Project-2/html/profile.html") {
+			if (location.pathname !== "/html/profile.html" || location.pathname === "/Noroff-Semester-Project-2/") {
 				home = "html/";
 			}
 
